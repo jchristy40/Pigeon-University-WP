a25005182cb0ab75a9eded46d14a33bb4c4f7312
John Christy <jchristy40@gmail.com>
2019-11-26 19:10:59 -0600

    add functionality for programs post type

--- app/public/wp-content/themes/pigeon-university-theme/single-program.php
+++ app/public/wp-content/themes/pigeon-university-theme/single-program.php
@@ -43,6 +43,10 @@
             )
           ));
 
+          if ($homepageEvents->have_posts()) {
+            echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
           while($homepageEvents->have_posts()) {
             $homepageEvents->the_post(); ?>
             <div class="event-summary">
@@ -63,6 +67,8 @@
           </div>
           </div>
           <?php }
+          }
+          
         ?>
 
   </div>
