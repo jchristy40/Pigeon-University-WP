ca8865786b0f20c4c46db0e280d5c6bd3bc8a190
John Christy <jchristy40@gmail.com>
2019-12-02 19:49:14 -0600

    wp update

--- app/public/wp-content/themes/pigeon-university-theme/front-page.php
+++ app/public/wp-content/themes/pigeon-university-theme/front-page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>);"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome, to All Bird Men!</h1>
-      <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="<?php echo get_post_type_archive_link('program); ?>" class="btn btn--large btn--blue">Find Your Major</a>
+      <h2 class="headline headline--medium">We think you'll like it here.</h2>
+      <h3 class="headline headline--small">Why don't you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
+      <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
     </div>
   </div>
 
@@ -19,7 +19,7 @@
           $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
             'posts_per_page' => 2,
-            'post_type' => 'event',
+            'post_type' => "event",
             'meta_key' => 'event_date',
             'orderby' => 'meta_value_num',
             'order' => 'ASC',
